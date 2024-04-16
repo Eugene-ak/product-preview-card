1e83b4fd14e20a18365c668d303608ac3932c6c7
Eugene-ak <amedioreugene@gmail.com>
2024-04-16 20:23:09 +0000

    Initial commit

---
+++ app/fonts.ts
@@ -0,0 +1,12 @@
+import { Montserrat, Fraunces } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  display: "swap",
+});
+export const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: "700",
+  display: "swap",
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,50 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import productImage from "@/public/images/image-product-mobile.jpg";
+import cartIcon from "@/public/images/icon-cart.svg";
+import { fraunces } from "./fonts";
+import style from "./home.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className={style.main}>
+        <section className={style.section}>
+          <Image src={productImage} className={style.image} alt="product" />
+        </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <section className={`${style.section} ${style.second_section}`}>
+          <h6 className={style.tag}>Perfume</h6>
+          <h1 className={`${fraunces.className} ${style.heading}`}>
+            Gabrielle Essence Eau De Parfum
+          </h1>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
           </p>
+
+          <div className={style.price_container}>
+            <span className={`${fraunces.className} ${style.price}`}>
+              $149.99
+            </span>{" "}
+            <s>$169.99</s>
+          </div>
+
+          <button className={style.cart_button}>
+            <span>
+              <Image src={cartIcon} alt="cart" />
+            </span>
+            Add to Cart
+          </button>
+        </section>
+      </main>
+
+      <footer className={style.footer}>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </div>
-    </main>
+        . Coded by <a href="#">Eugene Amedior</a>.
+      </footer>
+    </>
   );
 }
